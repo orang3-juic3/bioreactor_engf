e609177daa28808f7ab1b20b829d5c4fbf0cb17b
alex <alexzatstone@gmail.com>
2025-11-10 17:27:37 +0000

    initial

---
+++ bioreactor.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// For each subsystem, make three functions similar to this:
+void setupNameOfSubsystem() {
+  // Do not worry about other people using your pins for now we will figure this out later
+}
+void loopNameOfSubsystem() {
+  // this runs in the loop function
+}
+void getSetpoint() {
+
+}
+void setSetpoint() {
+  
+}
