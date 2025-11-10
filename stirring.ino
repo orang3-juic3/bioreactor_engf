7924cae79c7d99ab13edf63312dcc8f6588ef20f
alex <alexzatstone@gmail.com>
2025-11-10 17:35:43 +0000

    add files

---
+++ stirring.ino
@@ -0,0 +1,14 @@
+// Do not commit to the main branch !
+
+void setupStirring() {
+  // Do not worry about other people using your pins for now we will figure this out later
+}
+void loopStirring() {
+  // this runs in the loop function
+}
+void getTemperature() {
+
+}
+void setTemperatureSetpoint() {
+  
+}
