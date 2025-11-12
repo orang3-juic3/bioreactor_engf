29b78c0a4a6789211bb2205dc6a9685e5143abd1
mik-ali <54863165+mik-ali@users.noreply.github.com>
2025-11-12 12:11:20 +0000

    Add ph functionality skeleton

--- heating.ino
+++ heating.ino
@@ -1,16 +1,13 @@
 void setupHeating() {
-  // Do not worry about other people using your pins for now we will figure this out later
+  // Do not worry about other people using your pins for now we will figure this
+  // out later
 }
 void loopHeating() {
   // this runs in the loop function
 }
 double getTemperature() { // in Celsius
-
 }
-void setTemperatureSetpoint(double temperature) {
-  
-}
+void setTemperatureSetpoint(double temperature) {}
 
 double getHeaterEnergy() { // in Wh
-
 }
--- ph.ino
+++ ph.ino
@@ -1,12 +1,24 @@
-void setupHeating() {
-  // Do not worry about other people using your pins for now we will figure this out later
-}
-void loopHeating() {
-  // this runs in the loop function
-}
-void getTemperature() {
+// This is a skeleton written by a person who got no clue how the system
+// actually works
+// With this in mind, modify at your own discretion
 
-}
-void setTemperatureSetpoint() {
-  
-}
+// Should setup acid and base actuators
+void setupPHSystem() {}
+
+// Should set a setpoint for pH
+void setTargetPH(double targetPH) {}
+
+// Should return the current pH in the system
+double getPH() {}
+
+// Should add acid to the solution
+void addAcid() {}
+
+// Should add base to the solution
+void addBase() {}
+
+// Should return the acid actuator's current PWM
+double getAcidActuatorPWM() {}
+
+// Should return the base actuator's current PWM
+double getBaseActuatorPWM() {}
